edc821ae57e4e75cae165f3ac45855796f78df87
a.aguri <>
2020-06-05 00:25:16 +0900

    ブログの仮作成

---
+++ my-blog/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <div>
+      <v-app-bar color="deep-purple" dark>
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>かーくん勉強ブログ</v-toolbar-title>
+      </v-app-bar>
+    </div>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Twitter</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-content>
+      <HelloWorld />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld
+  },
+
+  data: () => ({
+    drawer: false
+  })
+};
+</script>
---
+++ my-blog/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto" max-width="344">
+      <Tweet id="1268497591857111040" :options="{ theme: 'light' }" />
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+
+          <v-card-text
+            >I'm a thing. But, like most politicians, he promised more than he
+            could deliver. You won't have time for sleeping, soldier, not with
+            all the bed making you'll be doing. Then we'll go with that data
+            file! Hey, you add a one and two zeros to that or we walk! You're
+            going to do his laundry? I've got to find a way to
+            escape.</v-card-text
+          >
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import { Tweet } from "vue-tweet-embed";
+
+export default {
+  name: "blog",
+  components: {
+    Tweet,
+  },
+  data: () => ({
+    show: false,
+    user_id: "2F1268497591857111040",
+  }),
+};
+</script>
